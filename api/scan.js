247396c849df63b419306988ef5e3288a50fde14
Tony <izipawsltd@gmail.com>
2026-06-10 09:31:55 +0930

    switch api to js

--- api/scan.js
+++ api/scan.js
@@ -1,6 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -23,7 +21,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     });
     const data = await response.json();
     return res.status(response.status).json(data);
-  } catch (e: any) {
+  } catch (e) {
     return res.status(500).json({ error: e.message });
   }
 }
